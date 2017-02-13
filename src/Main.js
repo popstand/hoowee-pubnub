bc3a9150a4c678af9866429af0127dcffe1d7ab2
plraphael <pedroluis.raphael@gmail.com>
2017-02-13 12:12:46 -0200

    capturing user audio with webrtc + some images

--- src/Main.js
+++ src/Main.js
@@ -2,23 +2,88 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
 
+import {
+  MediaStreamTrack,
+  getUserMedia,
+} from 'react-native-webrtc';
+
+
 export default class Main extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.initCall = this.initCall.bind(this);
+    this.getUserMediaCallback = this.getUserMediaCallback.bind(this);
+    this.errorCallback = this.errorCallback.bind(this);
+
+    this.state = {
+      callStatus: 'READY',
+    }
+  }
+
+  initCall() {
+    if(this.state.callStatus === 'ONGOING') return;
+    this.setState({
+      callStatus: 'ONGOING',
+    });
+
+    MediaStreamTrack.getSources(sourceInfos => {
+      getUserMedia({
+        audio: true,
+        video: false,
+      }, (stream) => {
+        console.log('got audio stream --->', stream);
+        this.getUserMediaCallback(stream);
+      }, this.errorCallback);
+    });
+  }
+
+  getUserMediaCallback(stream) {
+    console.log('stream --->', stream);
+  }
+
+  errorCallback(error) {
+    console.log('ERROR --->', error);
+  }
+
+  finishCall() {
+    if(this.state.callStatus === 'READY') return;
+    this.setState({
+      callStatus: 'READY',
+    });
+  }
+
   render() {
+    const {callStatus} = this.state;
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
+        <View style={styles.header}>
+          <Image source={require('../data/assets/images/hoowee-logo-blue.png')}/>
+        </View>
+
+        <View style={styles.body}>
+          <Text style={styles.instructions}>
+            To get started, click the dial image.
+          </Text>
+          {callStatus === 'READY' && <TouchableOpacity onPress={() => this.initCall()}>
+            <Image style={styles.dialImage} source={require('../data/assets/images/answer.png')}/>
+          </TouchableOpacity>}
+          {callStatus === 'ONGOING' && <TouchableOpacity onPress={() => this.finishCall()}>
+            <Image style={styles.dialImage} source={require('../data/assets/images/cancel.png')}/>
+          </TouchableOpacity>}
+        </View>
+
+        <View style={styles.footer}>
+        </View>
+
       </View>
     );
   }
@@ -31,14 +96,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  header: {
+    flex: 3,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  body: {
+    flex: 7,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footer: {
+    flex: 2,
   },
   instructions: {
+    fontSize: 14,
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 10,
   },
+  dialImage: {
+    width: 40,
+    resizeMode: 'contain',
+  }
 });
