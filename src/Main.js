f7e7a16d47802a593d9ab930bdb463528dadde81
plraphael <pedroluis.raphael@gmail.com>
2017-02-14 23:59:57 -0200

    rtc peer connection almost working

--- src/Main.js
+++ src/Main.js
@@ -12,6 +12,8 @@ import {
   MediaStreamTrack,
   getUserMedia,
   RTCView,
+  RTCSessionDescription,
+  RTCIceCandidate,
 } from 'react-native-webrtc';
 
 import {
@@ -24,41 +26,45 @@ const configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
 
 const CHANNEL_NAME = 'HooweeTestRoom';
 const JOIN = 'join';
+const EXCHANGE = 'exchange';
 
+let pcPeers = {};
 
 export default class Main extends Component {
 
   constructor(props){
     super(props);
 
-    this.pc1 = new RTCPeerConnection(configuration);
-    this.pc2 = new RTCPeerConnection(configuration);
+    this.pc = new RTCPeerConnection(configuration);
 
     this.initCall = this.initCall.bind(this);
-    this.getUserMediaCallback = this.getUserMediaCallback.bind(this);
+    this.getLocalMedia = this.getLocalMedia.bind(this);
     this.errorCallback = this.errorCallback.bind(this);
     this.onCreateOfferSuccess = this.onCreateOfferSuccess.bind(this);
     this.onCreateAnswerSuccess = this.onCreateAnswerSuccess.bind(this);
 
+    this.createPeerConnectionWithPublisher = this.createPeerConnectionWithPublisher.bind(this);
 
     this.subscribeToChannel = this.subscribeToChannel.bind(this);
     this.onPresenceChange = this.onPresenceChange.bind(this);
     this.onMessageReceived = this.onMessageReceived.bind(this);
-    this.onPublishMessage = this.onPublishMessage.bind(this);
+    this.publishJoinMessage = this.publishJoinMessage.bind(this);
 
     this.state = {
       callStatus: 'READY',
-      localStreamUrl: undefined,
-      remoteStreamUrl: undefined,
+      localStream: undefined,
+      remoteStream: undefined,
     }
   }
 
   async componentDidMount(){
-    console.log('COMPONENT DID MOUNT --->');
+    // console.log('COMPONENT DID MOUNT --->');
     await connect();
-    console.log('after connection to pubnub');
+    // console.log('after connection to pubnub');
 
     this.subscribeToChannel();
+
+    await this.getLocalMedia();
 
   }
 
@@ -87,91 +93,199 @@ export default class Main extends Component {
   }
 
   onMessageReceived(message) {
-    console.log('message --->', message);
+    // console.log('message --->', message.message);
+    if(message.message === JOIN) {
+      console.log('-------- CREATE PEER CONNECTION with publisher of the message');
+      const peerConnection = this.createPeerConnectionWithPublisher(message.published, true);
+      console.log('PEER CONNECTION --->', peerConnection);
+    }
+    if(message.message.message === EXCHANGE) {
+      console.log('----------- EXCHANGE MESSAGE RECEIVED', message.message);
+      this.exchange(message)
+    }
   }
 
-  onPublishMessage() {
+  async exchange(data) {
+    // console.log('EXCHANGE, DATA --->', data);
+
+    function logError(error) {
+      console.log("logError", error);
+    }
+
+    const fromId = data.publisher;
+
+    let pc;
+    if (fromId in pcPeers) {
+      // console.log('found pc --->', pcPeers[fromId]);
+      pc = pcPeers[fromId];
+    } else {
+      // console.log('pc didnt exist --->', pcPeers[fromId]);
+      pc = await this.createPeerConnectionWithPublisher(fromId, false);
+      console.log('PEER CONNECTION --->', pc);
+    }
+    // console.log('PEER CONNECTION --->', pc);
+    if (data.message.sdp) {
+      console.log('EXCHANGE SDP --->', data);
+      pc.setRemoteDescription(new RTCSessionDescription(data.message.sdp), function () {
+        if (pc.remoteDescription.type == "offer")
+          pc.createAnswer(function(desc) {
+            // console.log('createAnswer', desc);
+            pc.setLocalDescription(desc, async () => {
+              // console.log('setLocalDescription', pc.localDescription);
+              const message = {message: EXCHANGE, 'to': data.publisher, 'sdp': pc.localDescription};
+              await publishMessage(CHANNEL_NAME, message);
+            }, logError);
+          }, logError);
+      }, (error) => {console.log('SET REMOTE DESCRIPTION ERROR --->', error)});
+    } else {
+      // console.log('EXCHANGE CANDIDATE --->', data);
+      pc.addIceCandidate(new RTCIceCandidate(data.message.candidate));
+    }
+  }
+
+  async publishJoinMessage() {
     const {selectedChannel} = this.props;
 
     const channel = CHANNEL_NAME;
 
-    publishMessage(channel, JOIN)
-      .catch(error => {
-        console.error('Failed to publish message:', error);
+    try {
+      const publishPromise = await publishMessage(channel, JOIN);
+      console.log('publish promise --->', publishPromise);
+    } catch(error){
+      console.error('Failed to publish message --->', error);
+    };
+  }
+
+  getLocalMedia(){
+    console.log('getting local media');
+    return new Promise((resolve, reject) => {
+      MediaStreamTrack.getSources(sourceInfos => {
+        console.log('SOURCE INFOS --->', sourceInfos);
+        let videoSourceId;
+        for (const i = 0; i < sourceInfos.length; i++) {
+          const sourceInfo = sourceInfos[i];
+          if(sourceInfo.kind == "video" && sourceInfo.facing == "front") {
+            videoSourceId = sourceInfo.id;
+          }
+        }
+        getUserMedia({
+          audio: true,
+          video: {
+            mandatory: {
+              minWidth: 500, // Provide your own width, height and frame rate here
+              minHeight: 300,
+              minFrameRate: 30
+            },
+            facingMode: "user",
+            optional: (videoSourceId ? [{sourceId: videoSourceId}] : [])
+          }
+        }, (stream) => {
+          console.log('got stream -->', stream);
+          this.setState({
+            callStatus: 'ONGOING',
+            localStream: stream
+          }, () => resolve('success'));
+        }, () => reject('error'));
       });
+    });
   }
 
 
-
-
-
-  initCall() {
+  async initCall() {
     if(this.state.callStatus === 'ONGOING') return;
-    this.onPublishMessage();
+
+
+    console.log('AFTER LOCAL MEDIA ---->', this.state);
+
+    await this.publishJoinMessage();
+    console.log('AFTER PUBLISH MESSAGE ---->', this.state);
 
 
 
-    // MediaStreamTrack.getSources(sourceInfos => {
-    //   console.log(sourceInfos);
-    //   let videoSourceId;
-    //   for (const i = 0; i < sourceInfos.length; i++) {
-    //     const sourceInfo = sourceInfos[i];
-    //     if(sourceInfo.kind == "video" && sourceInfo.facing == "front") {
-    //       videoSourceId = sourceInfo.id;
-    //     }
-    //   }
-    //   getUserMedia({
-    //     audio: true,
-    //     video: {
-    //       mandatory: {
-    //         minWidth: 500, // Provide your own width, height and frame rate here
-    //         minHeight: 300,
-    //         minFrameRate: 30
-    //       },
-    //       facingMode: "user",
-    //       optional: (videoSourceId ? [{sourceId: videoSourceId}] : [])
-    //     }
-    //   }, (stream) => {
-    //     console.log('dddd', stream);
-    //     this.getUserMediaCallback(stream);
-    //   }, this.errorCallback);
-    // });
   }
 
-  getUserMediaCallback(stream) {
-    console.log('stream --->', stream);
-    this.setState({
-      callStatus: 'ONGOING',
-      localStreamUrl: stream.toURL()
-    }, () => this.initConnectionToPeers(stream))
-  }
+
 
   errorCallback(error) {
     console.log('ERROR --->', error);
   }
 
 
-  async initConnectionToPeers(stream) {
-    const {localStreamUrl} = this.state;
-    if(!localStreamUrl) return;
+  async createPeerConnectionWithPublisher(publisherId, isOffer) {
+    const pc = new RTCPeerConnection(configuration);
+    pcPeers[publisherId] = pc;
+    function logError(error) {
+      console.log("logError", error);
+    }
 
-    this.pc1.addStream(stream);
-
-    this.pc2.onaddstream = (event) => {
-      console.log('on add stream', event.stream);
-      this.setState({
-        remoteStreamUrl: event.stream.toURL(),
-      })
+    pc.onicecandidate = async (event) => {
+      // console.log('onicecandidate', event.candidate);
+      if (event.candidate) {
+        const message = {message: EXCHANGE, 'to': publisherId, 'candidate': event.candidate };
+        await publishMessage(CHANNEL_NAME, message);
+      }
     };
 
-    this.pc1.createOffer(
-      this.onCreateOfferSuccess,
-      (error) => {
-        console.log('offer error --->', error);
+    function createOffer() {
+      pc.createOffer(function(desc) {
+        // console.log('createOffer', desc);
+        pc.setLocalDescription(desc, async () => {
+          // console.log('setLocalDescription', pc.localDescription);
+          const message = {message: EXCHANGE, 'to': publisherId, 'sdp': pc.localDescription };
+          await publishMessage(CHANNEL_NAME, message);
+        }, logError);
+      }, logError);
+    }
+
+    pc.onnegotiationneeded = function () {
+      // console.log('onnegotiationneeded');
+      if (isOffer) {
+        createOffer();
       }
-    );
+    }
 
+    function getStats() {
+      if (pc.getRemoteStreams()[0] && pc.getRemoteStreams()[0].getAudioTracks()[0]) {
+        const track = pc.getRemoteStreams()[0].getAudioTracks()[0];
+        console.log('GET STATS --->', track);
+        pc.getStats(track, function(report) {
+          console.log('getStats report', report);
+        }, logError);
+      }
+    }
 
+    function createDataChannel() {
+      // console.log('create data channel --->');
+    }
+
+    pc.oniceconnectionstatechange = function(event) {
+      // console.log('oniceconnectionstatechange', event.target.iceConnectionState);
+      if (event.target.iceConnectionState === 'completed') {
+        setTimeout(() => {
+          getStats();
+        }, 1000);
+      }
+      if (event.target.iceConnectionState === 'connected') {
+        createDataChannel();
+      }
+    };
+
+    pc.onsignalingstatechange = function(event) {
+      // console.log('onsignalingstatechange', event.target.signalingState);
+    };
+
+    pc.onaddstream = (event) => {
+      console.log('on add stream --->', event.stream);
+      this.setState({remoteStream: event.stream});
+    };
+
+    pc.onremovestream = function (event) {
+      console.log('onremovestream', event.stream);
+    };
+
+    pc.addStream(this.state.localStream);
+
+    return pc;
   }
 
   onCreateOfferSuccess(desc) {
@@ -239,7 +353,7 @@ export default class Main extends Component {
     if(this.state.callStatus === 'READY') return;
     this.setState({
       callStatus: 'READY',
-      localStreamUrl: undefined,
+      localStream: undefined,
     });
   }
 
@@ -265,8 +379,8 @@ export default class Main extends Component {
           </TouchableOpacity>}
 
           <View style={styles.videos}>
-            <RTCView streamURL={this.state.localStreamUrl} style={styles.video}/>
-            <RTCView streamURL={this.state.remoteStreamUrl} style={styles.video}/>
+            <RTCView streamURL={this.state.localStream? this.state.localStream.toURL() : undefined} style={styles.video}/>
+            <RTCView streamURL={this.state.remoteStream ? this.state.remoteStream.toURL() : undefined} style={styles.video}/>
           </View>
         </View>
 
